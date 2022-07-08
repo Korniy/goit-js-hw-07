0b638ade8a4de247c12d03a592f28e3c07329e0f
Volodymyr Korniychuk <korniychuk49@gmail.com>
2022-07-08 19:21:34 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function creatingGallery(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+       return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+};
+
+galleryEl.insertAdjacentHTML('beforeend', creatingGallery(galleryItems));
+
